0970e3bc2dc24df4d05d0b00bf38c2ee92ff4ed6
5teve5 <47039007+5teve5@users.noreply.github.com>
2019-10-01 16:01:02 +0100

    Updates

---
+++ precache-manifest.d60475c16e611614b4690d35bf2a6489.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19bab75e3ee8c5c10f232010c01602c",
+    "url": "/Pokedex-app/index.html"
+  },
+  {
+    "revision": "3157f546b607c14ecadc",
+    "url": "/Pokedex-app/static/css/main.5474c8a7.chunk.css"
+  },
+  {
+    "revision": "7e6b11871b63ad104a69",
+    "url": "/Pokedex-app/static/js/2.a62c1e96.chunk.js"
+  },
+  {
+    "revision": "3157f546b607c14ecadc",
+    "url": "/Pokedex-app/static/js/main.3e679caa.chunk.js"
+  },
+  {
+    "revision": "04b63b4aa376cc8f4040",
+    "url": "/Pokedex-app/static/js/runtime-main.5c5f7d4e.js"
+  }
+]);
